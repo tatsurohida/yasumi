297761260b3715a84cd153946f25e36ce0a057bc
Tatsuro <tatsurohida@gmail.com>
2018-04-19 21:30:23 +0900

    controller

--- bot.js
+++ bot.js
@@ -9,11 +9,13 @@ clientSecret: process.env.BOTKIT_SLACK_CLIENT_SECRET,
 scopes: ['commands']
 });
 
+console.log(controller);
+
 controller.setupWebserver(process.env.PORT, function(err, webserver) {
 controller.createWebhookEndpoints(controller.webserver);
 controller.createOauthEndpoints(controller.webserver, function(err, req, res) {
 if (err) {
-res.status(509).send('Error: ' + JSON.stringify(err));
+res.status(500).send('Error: ' + JSON.stringify(err));
 } else {
 res.send('Success');
 }
